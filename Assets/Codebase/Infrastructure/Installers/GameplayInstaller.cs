781ba6244e9edaffda881c6e1da628458459c055
Alexey Potory <alexey.potory@gmail.com>
2024-12-15 12:36:30 +0500

    Added new input system, final project

--- Assets/Codebase/Infrastructure/Installers/GameplayInstaller.cs
+++ Assets/Codebase/Infrastructure/Installers/GameplayInstaller.cs
@@ -13,13 +13,14 @@ namespace Codebase.Infrastructure.Installers
     {
         [Header("Prefabs")]
         [SerializeField] private GameOverScreen _gameOverScreenPrefab;
-        
+
         [Header("Scene objects")]
         [SerializeField] private GameplayCameraHandlerBehaviour _cameraHandler;
-        
+        [SerializeField] private NewPlayerInput _playerInput;
+
         public override void InstallBindings()
         {
-            Container.Bind<IPlayerInput>().To<StandardPlayerInput>().AsTransient();
+            Container.Bind<IPlayerInput>().FromInstance(_playerInput).AsSingle();
             Container.Bind<IGameplayCameraHandler>().To<GameplayCameraHandlerBehaviour>().FromInstance(_cameraHandler).AsSingle();
             Container.Bind<IGameOverScreen>().To<GameOverScreen>().FromComponentInNewPrefab(_gameOverScreenPrefab).AsSingle().OnInstantiated<GameOverScreen>((_, obj) => obj.gameObject.SetActive(false)).NonLazy();
             Container.BindInterfacesAndSelfTo<GameLoop>().AsSingle().NonLazy();
---
+++ Assets/Codebase/Logic/Input/NewPlayerInput.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Codebase.Logic.Input
+{
+    public class NewPlayerInput : MonoBehaviour, IPlayerInput
+    {
+        [SerializeField] private InputActionAsset _inputAction;
+        
+        private InputAction _moveAction;
+        private InputAction _singleShotAction;
+        private InputAction _burstAction;
+
+        private void Start()
+        {
+            _moveAction = _inputAction.FindAction("Move");
+            _moveAction.Enable();
+            
+            _singleShotAction = _inputAction.FindAction("SingleShot");
+            _singleShotAction.Enable();
+            
+            _burstAction = _inputAction.FindAction("Burst");
+            _burstAction.Enable();
+        }
+
+        public float? MovementDirection => GetDirection();
+        public bool IsSingleShot => _singleShotAction.WasPerformedThisFrame();
+        public bool IsBursting => _burstAction.ReadValue<float>() > 0;
+
+        private float? GetDirection()
+        {
+            var direction = _moveAction.ReadValue<float>();
+
+            if (Mathf.Abs(direction) < Mathf.Epsilon)
+                return null;
+
+            return direction;
+        }
+    }
+}
